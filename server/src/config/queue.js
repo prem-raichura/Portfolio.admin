4e6796069f9d6d7f71bc5f3e6157392c3ab4fe93
Prem Raichura <premraichura7@gmail.com>
2026-05-23 23:23:24 +0530

    Queue.js file updated

--- server/src/config/queue.js
+++ server/src/config/queue.js
@@ -11,3 +11,22 @@ export const queueConnection =
         null,
     }
   );
+
+
+// import IORedis
+// from "ioredis";
+
+// export const queueConnection =
+//   new IORedis({
+
+//     host:
+//       process.env.REDIS_HOST ||
+//       "redis",
+
+//     port:
+//       process.env.REDIS_PORT ||
+//       6379,
+
+//     maxRetriesPerRequest:
+//       null,
+//   });
