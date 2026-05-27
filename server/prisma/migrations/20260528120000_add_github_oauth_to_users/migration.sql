44ce09e84b1661315d65999a271ee8fb51b384f7
Prem Raichura <premraichura7@gmail.com>
2026-05-28 00:48:41 +0530

    github authorization added

--- server/prisma/migrations/20260528120000_add_github_oauth_to_users/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "github_id" VARCHAR(100);
-
--- CreateIndex
-CREATE UNIQUE INDEX IF NOT EXISTS "User_github_id_key" ON "User"("github_id");
